06f9107bb1e3a76ae1554e456382dbd724a0f614
Ubuntu <azureuser@hom-devtest.353ik3j1kqkejmntl5jzpjqbug.bx.internal.cloudapp.net>
2024-03-26 11:47:14 +0000

    :boom: feat: Refactor routes and add new endpoints for apparel and product background change

--- routes/comfy.py
+++ routes/comfy.py
@@ -17,7 +17,7 @@ class BackgroundLora(BaseModel):
     strength_model: int
     strength_clip: int
 
-class BackgroundChangeRequest(BaseModel):
+class ApparelBackgroundChangeRequest(BaseModel):
     positivePrompt: str
     negativePrompt: str
     backgroundLora: Optional[BackgroundLora] = None
@@ -29,23 +29,23 @@ class BackgroundChangeRequest(BaseModel):
     outputCount: Optional[int] = 1
     seed: Optional[int] = None
 
-@comfy.route('/api/comfy/background-change', methods=['POST'])
-def backgroundChange():
+@comfy.route('/api/image-generation/apparel', methods=['POST'])
+def apparelBackgroundChange():
     try:
         authenticate(request.headers.get('Authorization'))
         shouldUseLora = request.args.get('lora', False).lower() == 'true'
         client_id = request.args.get('clientId', None)
-        request_data = BackgroundChangeRequest.parse_obj(request.json)
+        request_data = ApparelBackgroundChangeRequest.parse_obj(request.json)
         
         if not request_data.seed:
-            request_data.seed = random.randint(0, 1_500_000)
+            request_data.seed = random.randint(1, 1_500_000)
         
         workflow = None
         base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         if shouldUseLora:
-            file_path = os.path.join(base_dir, "workflow", "background_change", "bg_change_with_background_lora.json")
+            file_path = os.path.join(base_dir, "workflow", "apparel_background_change", "bg_change_with_background_lora.json")
         else:          
-            file_path = os.path.join(base_dir, "workflow", "background_change", "bg_change_gaussian_blur.json")
+            file_path = os.path.join(base_dir, "workflow", "apparel_background_change", "bg_change_gaussian_blur.json")
             
         with open(file_path, "r") as f:
             workflow = json.load(f)        
@@ -68,6 +68,62 @@ def backgroundChange():
         
         workflow["115"]["inputs"]["amount"] = request_data.outputCount
         workflow["45"]["inputs"]["filename_prefix"] = request_data.outputFileName
+        
+        data = {
+            "prompt": workflow,
+            "client_id": client_id
+        }
+        
+        try:
+            comfy_url = f'{os.getenv("COMFY_URL")}/prompt'
+            response = requests.post(comfy_url, json=data)
+            
+            return jsonify(response.json()), response.status_code
+        except requests.RequestException as e:
+            return jsonify({"error": str(e)}), 400
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+  
+
+class ProductBackgroundChangeRequest(BaseModel):
+    prompt: str
+    inputProductImagePath: str
+    backgroundRefImagePath: str
+    inputBlackAndWhiteImagePath: str
+    inputFocusImagePath: str
+    outputFileName: str
+    renderStrength: float
+    seed: Optional[int] = None
+    
+@comfy.route('/api/image-generation/product', methods=['POST'])
+def productBackgroundChange():
+    try:
+        authenticate(request.headers.get('Authorization'))
+        client_id = request.args.get('clientId', None)
+        request_data = ProductBackgroundChangeRequest.parse_obj(request.json)
+        
+        if not request_data.seed:
+            request_data.seed = random.randint(1, 1_500_000)
+        
+        workflow = None
+        base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        file_path = os.path.join(base_dir, "workflow", "product_background_change", "bg_change_product.json")
+            
+        with open(file_path, "r") as f:
+            workflow = json.load(f)        
+        
+        workflow["41"]["inputs"]["seed"] = request_data.seed
+        
+        workflow["144"]["inputs"]["prompt"] = request_data.prompt
+        workflow["121"]["inputs"]["weight"] = request_data.renderStrength
+        
+        workflow["94"]["inputs"]["image"] = request_data.inputProductImagePath
+        workflow["124"]["inputs"]["image"] = request_data.backgroundRefImagePath
+        workflow["131"]["inputs"]["image"] = request_data.inputFocusImagePath
+        workflow["156"]["inputs"]["image"] = request_data.inputBlackAndWhiteImagePath
+        
+        workflow["106"]["inputs"]["filename_prefix"] = request_data.outputFileName
         
         data = {
             "prompt": workflow,
